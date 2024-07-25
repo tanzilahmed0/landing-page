d2c049d60bcf9de1f59c7177f77e656ed93a69f0
tanzilahmed0 <tanahmed03@gmail.com>
2024-07-24 23:37:47 -0700

    Added transition and reduced-motion support to .wrapper class

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,4 +142,18 @@
 
 			});
 
-})(jQuery);
+			const observer = new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					console.log(entry)
+					if (entry.isIntersecting) {
+						entry.target.classList.add('show');
+					} else {
+						entry.target.classList.remove('show');
+					}
+				});
+
+			
+			const hiddenElements = document.querySelectorAll('.scroll-animation');
+			hiddenElements.forEach((el) => observer.observe(el));
+
+})(jQuery)});
